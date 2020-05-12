b43a64dd9327072b50065272110609a5b1e05cd4
Jorge da Silva Guimarães <jorgeguimaraes@cinbesa.com.br>
2020-05-11 22:49:38 -0300

    refactor authentication and configuration

--- auth/src/main/java/academy/devdojo/youtube/auth/security/config/filter/JwtUsernameAndPasswordAuthenticationFilter.java
+++ auth/src/main/java/academy/devdojo/youtube/auth/security/config/filter/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -2,41 +2,24 @@ package academy.devdojo.youtube.auth.security.config.filter;
 
 import academy.devdojo.youtube.core.model.ApplicationUser;
 import academy.devdojo.youtube.core.property.JwtConfiguration;
+import academy.devdojo.youtube.security.token.creator.TokenCreator;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nimbusds.jose.*;
-import com.nimbusds.jose.crypto.DirectEncrypter;
-import com.nimbusds.jose.crypto.RSASSASigner;
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.mapping.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.interfaces.RSAPublicKey;
-import java.util.Date;
-import java.util.UUID;
-
-import static java.util.Collections.*;
-import static java.util.stream.Collectors.toList;
+import static java.util.Collections.emptyList;
 
 
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -44,6 +27,7 @@ import static java.util.stream.Collectors.toList;
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
     private final JwtConfiguration jwtConfiguration;
+    private final TokenCreator tokenCreator;
 
     @Override
     @SneakyThrows
@@ -65,12 +49,12 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
 
     @Override
     @SneakyThrows
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication auth) throws IOException, ServletException {
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication auth)  {
         log.info("Authentication was successful for the user '{}', generating JWE token", auth.getName());
 
-        SignedJWT signedJWT = createSignedJWT(auth);
+        SignedJWT signedJWT = tokenCreator.createSignedJWT(auth);
 
-        String encryptedToken = encrypToken(signedJWT);
+        String encryptedToken = tokenCreator.encrypToken(signedJWT);
 
         log.info("Token genereted successfully, adding it to the response header");
 
@@ -79,70 +63,5 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
 
     }
 
-    @SneakyThrows
-    private SignedJWT createSignedJWT(Authentication auth){
-        log.info("Starting to create the signed JWT");
-
-        ApplicationUser applicationUser =  (ApplicationUser) auth.getPrincipal();
-        JWTClaimsSet jwtClaimsSet =  createJWTClaimSet(auth, applicationUser);
-
-        KeyPair rsaKey = generateKeyPair();
-
-        log.info("Building JWK from the RSA Keys");
-
-        JWK jwk = new RSAKey.Builder((RSAPublicKey) rsaKey.getPublic()).keyID(UUID.randomUUID().toString()).build();
-
-        SignedJWT signedJWT = new SignedJWT(new JWSHeader.Builder(JWSAlgorithm.RS256).jwk(jwk).type(JOSEObjectType.JWT).build(), jwtClaimsSet);
-
-        log.info("Signing the token with the private RSA Key");
-
-        RSASSASigner signer = new RSASSASigner(rsaKey.getPrivate());
-
-        signedJWT.sign(signer);
-
-        log.info("Serialized token '{}'", signedJWT.serialize());
-
-        return signedJWT;
-
-    }
-
-
-    private JWTClaimsSet createJWTClaimSet(Authentication auth, ApplicationUser applicationUser){
-        log.info("Creating the ClaimSet Object for '{}'", applicationUser);
-
-        return new JWTClaimsSet.Builder()
-                .subject(applicationUser.getUsername())
-                .claim("authorities", auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(toList()))
-                .issuer("http://academy.devdojo")
-                .issueTime(new Date())
-                .expirationTime(new Date(System.currentTimeMillis() + (jwtConfiguration.getExpiration() * 1000)))
-                .build();
-    }
-
-
-    @SneakyThrows
-    private KeyPair generateKeyPair(){
-        log.info("Generating RSA 2048 bits Keys");
-
-        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-        generator.initialize(2048);
-        return generator.genKeyPair();
-    }
-
-
-    private String encrypToken(SignedJWT signedJWT) throws JOSEException {
-        log.info("Starting the encryptToken method");
-        DirectEncrypter directEncrypter = new DirectEncrypter(jwtConfiguration.getPrivateKey().getBytes());
-
-        JWEObject jwtObject = new JWEObject(new JWEHeader.Builder(JWEAlgorithm.DIR, EncryptionMethod.A128CBC_HS256).contentType("JWT").build(), new Payload(signedJWT));
-
-        log.info("Encrypting token with system's private key ");
-
-        jwtObject.encrypt(directEncrypter);
-
-        log.info("Token encrypted");
-
-        return jwtObject.serialize();
-    }
 
 }
